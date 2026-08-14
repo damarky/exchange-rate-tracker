183df3db94c0db3f2001097c2264faf34168b03d
david.mjf <77984941+damarky@users.noreply.github.com>
2026-08-14 23:26:08 +0800

    Store fetched results

--- fetch_rates.py
+++ fetch_rates.py
@@ -1,11 +1,24 @@
+import sqlite3
+
 import requests
 
+conn = sqlite3.connect('rates.db')
+cursor = conn.cursor()
+
 response = requests.get("https://api.frankfurter.dev/v1/latest?base=USD&symbols=JPY,PHP")
-# print(response.status_code)
-# print(response.json())
+
 data = response.json()
 
+date = data['date']
+base_currency = data['base']
+
 for key, value in data['rates'].items():
-    print(key, value)
-print(data['base'])
-print(data['date'])
+    target_currency = key
+    rate = value
+    cursor.execute(
+        "INSERT OR IGNORE INTO rates (date, base_currency, target_currency, rate) VALUES (?, ?, ?, ?)",
+        (date, base_currency, target_currency, rate)
+    )
+
+conn.commit()
+conn.close()
